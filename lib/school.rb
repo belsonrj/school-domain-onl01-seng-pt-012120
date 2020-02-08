d49c170e4ea3eee815129f657d4a003cfd7a7192
Rick Belson <belsonrj@aol.com>
2020-02-08 20:56:37 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -25,3 +25,8 @@ class School
 end 
 
 school.add_student("Blake Johnson", 7)
+school.add_student("Jack Bauer", 7)
+school.add_student("Bart Simpson", 9)
+school.add_student("Homer Simpson", 9)
+school.add_student("Avi Flombaum", 10)
+school.add_student("Jeff Baird", 10)
