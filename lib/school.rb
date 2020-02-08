532b7d3a69193d918363b265a3efddc45f7ecef1
Rick Belson <belsonrj@aol.com>
2020-02-08 20:42:53 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -19,6 +19,14 @@ class School
       nu_hash[x] = y.sort 
     end 
     nu_hash
+  end
+  
+  def grade(level)
+    roster.detect do |x, y| 
+      if x == level
+        return y 
+    end 
   end 
 
 end
+
