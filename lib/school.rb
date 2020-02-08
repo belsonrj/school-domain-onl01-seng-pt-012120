70c28eb19c9f74050de1f8499938e38fe6a8feee
Rick Belson <belsonrj@aol.com>
2020-02-08 20:33:56 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,5 +1,8 @@
-class SCHOOL
-  school.roster = {}
-end
-
-school = School.new("Bayside High School")
+class School
+  attr_accessor :name, :roster
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end 
+  
