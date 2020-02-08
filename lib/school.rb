d4f62d735fbbe94ada87a0833d612101db121df5
Rick Belson <belsonrj@aol.com>
2020-02-08 21:35:07 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -17,15 +17,3 @@ class School
 
 end
 
-school = School.new("Bayside High School")
-school.roster
-school.add_student("Zach Morris", 9)
-school.add_student("Moshe Zeus", 9)
-school.add_student("Onkelos Octavius", 9)
-school.roster
-school.add_student("Sugar Zaza", 5)
-school.roster
-school.add_student("Theo Madus", 5)
-school.add_student("Abbah Zabbah", 2)
-school.roster
-school.sort(grade)
