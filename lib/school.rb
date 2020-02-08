5b961cd2b99a1ad1aea43da6b15a2adfb1802368
Rick Belson <belsonrj@aol.com>
2020-02-08 20:35:48 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -13,12 +13,12 @@ class School
     roster[level] << student
   end 
 
-def sort 
-  nu_hash = {}
-  roster.each do |x, y| 
-    nu_hash[x] = y.sort 
+  def sort 
+    nu_hash = {}
+    roster.each do |x, y| 
+      nu_hash[x] = y.sort 
+    end 
+    nu_hash
   end 
-  nu_hash
-end 
 
 end
