56e9cbc99ccc9ab8d8e662e6a806026b7da89240
Rick Belson <belsonrj@aol.com>
2020-02-08 20:46:51 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -6,11 +6,11 @@ class School
     @roster = {}
   end 
 
-  def add_student(student, grade)
+  def add_student(student, level)
     # this sort of means #=> a || a = b ... or #=> x || x = y
     # it's freak'n weird
-    roster[grade] ||= []
-    roster[grade] << student
+    roster[level] ||= []
+    roster[level] << student
   end 
 
   def sort 
